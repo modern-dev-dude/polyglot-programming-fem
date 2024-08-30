0b438b27662be9c00c3a6647408f866320f67b18
Jason S <jstodmdd@amazon.com>
2024-08-30 11:45:51 -0400

    added submarine problem

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,18 +1,3 @@
-enum RSEnum {
-    FOO(i32),
-    FOO2(Option<i32>),
-    BAR(String),
-    BAZ(Vec<String>),
-}
-
 fn main() {
-    let foo = RSEnum::FOO2(Some(5));
-
-    if let RSEnum::FOO(value) = foo {
-        print!("foo: {}", value);
-    }
-
-    if let RSEnum::FOO2(Some(value)) = foo {
-        print!("foo2: {}", value);
-    }
+    println!("hello");
 }
