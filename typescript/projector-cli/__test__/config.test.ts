9d0570d6d542b8dd6857b5f77b17cd42ebb80eb3
Jason S <jstodmdd@amazon.com>
2024-09-17 18:18:48 -0400

    added ts unit test

---
+++ typescript/projector-cli/__test__/config.test.ts
@@ -0,0 +1,35 @@
+import getConfig, { Operation } from "../src/config";
+
+test("simple print all", function () {
+  const config = getConfig({});
+
+  expect(config.operation).toEqual(Operation.Print);
+  expect(config.args).toEqual([]);
+});
+
+test("simple print key", function () {
+  const config = getConfig({
+    args: ["foo"],
+  });
+
+  expect(config.operation).toEqual(Operation.Print);
+  expect(config.args).toEqual(["foo"]);
+});
+
+test("add key", function () {
+  const config = getConfig({
+    args: ["add", "foo", "bar"],
+  });
+
+  expect(config.operation).toEqual(Operation.Add);
+  expect(config.args).toEqual(["foo", "bar"]);
+});
+
+test("rm key", function () {
+  const config = getConfig({
+    args: ["rm", "foo"],
+  });
+
+  expect(config.operation).toEqual(Operation.Remove);
+  expect(config.args).toEqual(["foo"]);
+});
